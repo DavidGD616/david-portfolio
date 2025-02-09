e0cace5e1a270666bd27b6858164aa5869b8c86c
DavidGD616 <davidguerrerodiaz12@gmail.com>
2025-02-08 21:24:36 -0800

    robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import robotsTxt from 'astro-robots-txt';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://davidguerrero.co',
+  integrations: [robotsTxt(), sitemap()],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   }
 });
